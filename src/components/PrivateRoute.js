b3c6c5c6cab82f08737e39113e8a9b64ea0a7087
Alex Orlet <AlexOrlet89@gmail.com>
2022-05-06 10:14:03 -0700

    some security is added to our private route: user must be true in order to render home page, else redirected to /login

--- src/components/PrivateRoute.js
+++ src/components/PrivateRoute.js
@@ -1,3 +1,4 @@
+import { Redirect } from 'react-router-dom';
 import { Route } from 'react-router-dom';
 import { useUserContext } from '../context/UserContext';
 
@@ -5,5 +6,12 @@ export function PrivateRoute({ children, ...rest }) {
   //do we need this??
   const { user } = useUserContext();
 
-  return <Route {...rest}>{children}</Route>;
+  return (
+    <Route
+      {...rest}
+      render={() =>
+        user ? children : <Redirect to={{ pathname: '/login' }} />
+      }
+    />
+  );
 }
